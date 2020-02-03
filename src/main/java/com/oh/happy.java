99bbc5414ff9e4e5fa18d6fcf2ee27e1b15a0d2d
atom <atom@qq.com>
2020-02-03 17:27:29 +0800

    hot fix 0000

--- src/main/java/com/oh/happy.java
+++ src/main/java/com/oh/happy.java
@@ -3,6 +3,6 @@ package com.oh;
 public class happy {
 	public static void main(String[] args) {
 		System.out.println("left .....");
-		System.out.println("hot fix .....");
+		System.out.println("hot fix 0000.....");
 	}
 }
